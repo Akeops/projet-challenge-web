dddad97acb07b9708b17804574d91c11454ac003
pierrelepottier <pierre@lepottier.fr>
2023-12-22 09:01:20 +0100

    PIERRE

--- views/partials/_header.php
+++ views/partials/_header.php
@@ -1,6 +1,9 @@
 <header>
 	<nav>
 		<a href="../../index.php?page=home">Accueil</a>
+        <a href="../../index.php?page=directory">Annuaire</a>
+        <a href="../../index.php?page=forum">Forum</a>
         <a href="../../index.php?page=register">Inscription</a>
+        <a href="../../index.php?page=login">Connexion</a>
 	</nav>
 </header>

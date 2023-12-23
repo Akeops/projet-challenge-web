696e9c0f78efef809eb1a3d657098ea889e34437
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-12-23 16:10:36 +0100

    Mise à jour du css

--- views/pages/home.php
+++ views/pages/home.php
@@ -3,14 +3,36 @@
     <!-- Présentation de la LOW TECH -->
     <section id="presentation">
         <div class="section-title">
-            <h2 class="sectiion-title">Présentation de l'association LOW TECH Bordeaux</h2>
+            <h2>Présentation de l'association LOW TECH Bordeaux</h2>
         </div>
         <div class="divPresentation">
             <div class="presentation">
                 <img src="./img/lowTechPresentation.PNG" class="" alt="photo de moi">
             </div>
             <div class="presentationDescription">
-                    <p>Nous sommes une association qui croit au pouvoir de l’innovation utile, accessible et durable pour répondre aux enjeux d’aujourd’hui et de demain : les low-technologies offrent à chacun et partout, les moyens de répondre à ses besoins dans le respect des Humains et de la Planète !</p>
+                <p>Nous sommes une association qui croit au pouvoir de l’innovation utile, accessible et durable pour répondre aux enjeux d’aujourd’hui et de demain : les low-technologies offrent à chacun et partout, les moyens de répondre à ses besoins dans le respect des Humains et de la Planète !
+                <br>
+                <br>
+                
+                <b>Sensibilisation:</b><br>
+                    Organisation d’actions de sensibilisation auprès du grand public, scolaires, collectivités & entreprises du territoire.
+                    Animation de la communauté avec des évènements, des conférences, des sorties, des apéros, etc.
+
+                <br>
+                <br>
+                <b>Makerspace:</b><br>
+                    Centre de pratique avec outillages professionnels mutualisés, formations, stages et ateliers.
+                    Encadrement d’étudiants de l’enseignement supérieur.
+                    Espace coworking pour les entrepreneurs avec une démarche low-tech.
+
+                <br>
+                <br>
+
+                <b>Bureau d’étude:</b><br>
+                    Mettre la low-tech au service de l’innovation avec son pôle R&D.
+                    Favoriser l’émergence de solutions low-techs auprès des collectivités & entreprises.
+                    Diffuser en open source avec des outils collaboratifs.
+                </p>
             </div>
         </div>
     </section>

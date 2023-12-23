fcfce49c915ff9582981f5fe9d879088d20ff34d
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-12-23 15:36:27 +0100

    Création branche css

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,45 +1,17 @@
 <div class="home">
 
-	<section id="about">
-		<h1>Bienvenue sur la présentation du projet Tiny house ! </h1>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-	</section>
+    <!-- Présentation de la LOW TECH -->
+    <section id="presentation">
+        <div class="section-title">
+            <h2 class="sectiion-title">Présentation de l'association LOW TECH Bordeaux</h2>
+        </div>
+        <div class="divPresentation">
+            <div class="presentation">
+                <img src="./img/lowTechPresentation.PNG" class="" alt="photo de moi">
+            </div>
+            <div class="presentationDescription">
+                    <p>Nous sommes une association qui croit au pouvoir de l’innovation utile, accessible et durable pour répondre aux enjeux d’aujourd’hui et de demain : les low-technologies offrent à chacun et partout, les moyens de répondre à ses besoins dans le respect des Humains et de la Planète !</p>
+            </div>
+        </div>
+    </section>
 </div>

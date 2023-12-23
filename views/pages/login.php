4164e48f205e34f1b4a276dd7f9b49deba1a209c
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-12-23 09:48:44 +0100

    Meilleur agencement du formulaire de connexion

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,7 +1,9 @@
 <form action="index.php?page=login" method="POST">
 	<label for="email">Adresse email :</label>
 	<input type="email" name="email" id="login">
+
 	<label for="password">Mot de passe :</label>
 	<input type="password" name="password" id="password">
+	
 	<input type="submit" value="connexion">
 </form>

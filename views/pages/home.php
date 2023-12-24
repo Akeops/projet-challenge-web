a0329c0aa471881c09babfcff16649847b33c95d
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-12-24 15:07:09 +0100

    avancement css

--- views/pages/home.php
+++ views/pages/home.php
@@ -4,21 +4,21 @@
             <h2 class=titreH2>Présentation de l'association LOW TECH Bordeaux</h2>
         </div>
         <div class="divPresentation">
-            <div class="presentation">
-                <img src="./img/lowTechPresentation.PNG" class="" alt="photo de moi">
+            <div class="presentationImg">
+                <img src="./img/lowTechPresentation.PNG" alt="photo du logo LowTech">
             </div>
             <div class="presentationDescription">
                 <p>Nous sommes une association qui croit au pouvoir de l’innovation utile, accessible et durable pour répondre aux enjeux d’aujourd’hui et de demain : les low-technologies offrent à chacun et partout, les moyens de répondre à ses besoins dans le respect des Humains et de la Planète !
                 <br>
                 <br>
                 
-                <b>Sensibilisation:</b><br>
+                <b class="sous-titres">Sensibilisation:</b><br>
                     Organisation d’actions de sensibilisation auprès du grand public, scolaires, collectivités & entreprises du territoire.
                     Animation de la communauté avec des évènements, des conférences, des sorties, des apéros, etc.
 
                 <br>
                 <br>
-                <b>Makerspace:</b><br>
+                <b class="sous-titres">Makerspace:</b><br>
                     Centre de pratique avec outillages professionnels mutualisés, formations, stages et ateliers.
                     Encadrement d’étudiants de l’enseignement supérieur.
                     Espace coworking pour les entrepreneurs avec une démarche low-tech.
@@ -26,11 +26,71 @@
                 <br>
                 <br>
 
-                <b>Bureau d’étude:</b><br>
+                <b class="sous-titres">Bureau d’étude:</b><br>
                     Mettre la low-tech au service de l’innovation avec son pôle R&D.
                     Favoriser l’émergence de solutions low-techs auprès des collectivités & entreprises.
                     Diffuser en open source avec des outils collaboratifs.
                 </p>
+            </div>
+        </div>
+    </section>
+
+    <section id="activite">
+        <div class="section-title">
+            <h2 class=titreH2AvecFond>Nos activités</h2>
+        </div>
+        <div class="divPresentation">
+            <div class="presentationDescription2">
+                    <p>La low-tech nous semble être un outil majeur pour la transition écologique et énergétique, il est donc nécessaire que le plus de monde possible soit informé sur ce que c'est et quelles sont ses applications.
+                    <br>
+                    <br>
+                    À cette fin, nous pouvons intervenir pour une large variété de publics, allant des scolaires aux salariés d'entreprises dans des ateliers dédiés, en passant par les stands d'exposition et les conférences ou tables rondes.
+                    <br>
+                    <br>
+                    Notre atelier est lui aussi tourné vers la transmission des connaissances et de l'apprentissage de la démarche low-tech. Pour ce faire, nous organisons régulièrement des stages sur une thématique spécifique, et nous mettons petit à petit en place un catalogue de formation couvrant diverses thématiques pour augmenter votre autonomie et faire des économies. C'est aussi un lieu où se retrouvent les adhérents pour co-construire les projets de l'association et participer à ses missions d'intérêt général.
+                    <br>
+                    <br>
+                    Les low-techs que nous fabriquons sont conçues, construites et testées par le bureau de Recherche & Développement de l'association, pour être capable de vous fournir des données d'utilisation, de mieux comprendre le fonctionnement des low-techs fabriquées, et adapter les solutions aux besoins spécifiques du territoire. Toutes les low-techs proposées par l'association sont donc réellement utiles, elles participent concrètement à la résilience et l'autonomie des personnes et des structures collectives.
+                    </p>
+                </div>
+                <div class="presentationImg2">
+                    <img src="./img/planete.png" class="" alt="photo de planete jaune">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="projetTinyHouse">
+        <div class="section-title">
+            <h2 class=titreH2>Le projet Tiny House</h2>
+        </div>
+        <div class="divPresentation">
+            <div class="presentationImg">
+                <img src="./img/tinyHouses.PNG" alt="photo du logo LowTech">
+            </div>
+            <div class="presentationDescription">
+            <b class="sous-titres">Le projet</b>
+            <br>
+            <br>
+            L’association va, au cours des deux prochaines années, entreprendre un projet de construction de Tiny House équipées de Low-Techs, tels qu’un poële de masse, un garde manger, marmite norvégienne ou une jardinière autonome. 
+            <br>
+            <br>
+            Ce projet fera collaborer différentes associations, et la tiny sera exposée sur les quais de la Garonne à Bordeaux.
+            <br>
+            <br>
+            <b class="sous-titres">Les ojectifs</b>
+            <br>
+            <br>
+            <ul>
+                <li class="liObjectif">Sensibiliser le grand public autour des enjeux écologiques, l'éco-construction, de la low tech et du réemploi.</li>
+                <li class="liObjectif">Proposer des chantiers participatifs où chacun peut venir contriber ou apprendre à bricoler</li>
+                <li class="liObjectif">Produire une documentation, éventuellement sous forme d'un livre blanc open source pour attester du retour d'expérience et des performances du démonstrateur</li>
+                <li class="liObjectif" style="font-weight: 600;">**Experimentation d’une plateforme de mise en relation entre les différents acteurs, mécènes et bénévoles**</li>
+            </ul>
+            <br>
+            <br>
+            En créant un maillage des acteurs Low Tech sur le territoire, l’association veut devenir la vitrine du réseau Low Tech de la région, développer des partenariats avec les autres acteurs et créer une dynamique sur le territoire de la Nouvelle-Aquitaine.
+            
             </div>
         </div>
     </section>
